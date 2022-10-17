1f252c1ea9f6b4f9031e3fba04deb8b105e11b2d
dhk767 <dhk767@gmail.com>
2022-10-17 13:36:04 +0900

    초기 세팅 완료

---
+++ src/test/java/com/ll/mutbook/MutbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.mutbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
