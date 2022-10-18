cd5900b2ec52e0d0bebc116ee6e3fe3581c8efcf
dhk767 <dhk767@gmail.com>
2022-10-18 13:46:44 +0900

    feat : PasswordEncoder 객체 DI 방식으로 활용하는 방식으로 변경

--- src/main/java/com/ll/mutbook/user/UserService.java
+++ src/main/java/com/ll/mutbook/user/UserService.java
@@ -2,19 +2,20 @@ package com.ll.mutbook.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public SiteUser create(String username, String userNickname, String email, String passward){
         SiteUser user = new SiteUser();
         user.setUsername(username);
         user.setUserNickname(userNickname);
         user.setEmail(email);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         user.setPassword(passwordEncoder.encode(passward));
         this.userRepository.save(user);
 
